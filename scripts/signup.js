26c7b12f307bb325b4ccd46a5741b2301e2fbd43
johnmichaelespiritu <johnmichael.espiritu.pixel8@gmail.com>
2023-06-23 16:30:13 +0800

    Add functionality for 'Sign In' button.

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,47 +1,27 @@
-// var forgotPassword = document.querySelectorAll(".label-forgot-password");
-// var signUp = document.querySelectorAll(".label-sign-up");
-
-// forgotPassword.forEach(function (label) {
-//   label.addEventListener("click", function () {
-//     // Custom action to perform when label is clicked
-//     alert("Label clicked!");
-//   });
-// });
-
-// signUp.forEach(function (label) {
-//   label.addEventListener("click", function () {
-//     // Custom action to perform when label is clicked
-//     alert("Label clicked!");
-//   });
-// });
-
-var clickableLabels = document.querySelectorAll(".label-sign-in");
-
-clickableLabels.forEach(function (label) {
-  label.addEventListener("click", function () {
-    // Custom action to perform when label is clicked
-    alert("Label clicked!");
-  });
+document.addEventListener("click", function (event) {
+  if (event.target.matches("#sign-in")) {
+    window.location.href = "login.html";
+  }
 });
 
-var socialMediaLinks = document.querySelectorAll(".social-media-links img");
-socialMediaLinks.forEach(function (socialMedia) {
-  socialMedia.addEventListener("click", function () {
-    // Get the alt attribute of the clicked image
-    var altText = socialMedia.alt;
+// var socialMediaLinks = document.querySelectorAll(".social-media-links img");
+// socialMediaLinks.forEach(function (socialMedia) {
+//   socialMedia.addEventListener("click", function () {
+//     // Get the alt attribute of the clicked image
+//     var altText = socialMedia.alt;
 
-    // Perform different actions based on the alt text
-    switch (altText) {
-      case "google":
-        window.location.href = "https://www.google.com";
-        break;
-      case "facebook":
-        window.location.href = "https://www.facebook.com";
-        break;
-      case "apple":
-        window.location.href = "https://www.apple.com";
-        break;
-      // Add more cases for other alt texts if needed
-    }
-  });
-});
+//     // Perform different actions based on the alt text
+//     switch (altText) {
+//       case "google":
+//         window.location.href = "https://www.google.com";
+//         break;
+//       case "facebook":
+//         window.location.href = "https://www.facebook.com";
+//         break;
+//       case "apple":
+//         window.location.href = "https://www.apple.com";
+//         break;
+//       // Add more cases for other alt texts if needed
+//     }
+//   });
+// });
