2e84689c580dc2d9bb9531fbffd898b19e207979
johnmichaelespiritu <johnmichael.espiritu.pixel8@gmail.com>
2023-06-26 14:55:16 +0800

    Add functionality for add user account and validate the registration form.

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,6 +1,11 @@
-const baseURL =
-  "http://localhost/mobile-dev-finals/scripts/php/user-account.php";
 const signUpButton = document.getElementById("signup-button");
+
+const formFields = {
+  username: document.getElementById("username-input"),
+  email: document.getElementById("email-input"),
+  password: document.getElementById("password-input"),
+  confirmPassword: document.getElementById("confirm-password-input"),
+};
 
 document.addEventListener("click", function (event) {
   if (event.target.matches("#sign-in")) {
@@ -8,72 +13,120 @@ document.addEventListener("click", function (event) {
   }
 });
 
-function validateRegistrationForm() {
-  const inputForm = {
-    username: document.getElementById("username-input").value,
-    email: document.getElementById("email-input").value,
-    password: document.getElementById("password-input").value,
-    confirmPassword: document.getElementById("confirm-password-input").value,
+function getFormInputValues() {
+  return {
+    username: document.getElementById("username-input").value.trim(),
+    email: document.getElementById("email-input").value.trim(),
+    password: document.getElementById("password-input").value.trim(),
+    confirmPassword: document
+      .getElementById("confirm-password-input")
+      .value.trim(),
   };
-
-  if (inputForm.username === "") {
-    alert("Please enter a username.");
-    return false;
-  }
-
-  if (inputForm.email === "") {
-    alert("Please enter an email address.");
-    return false;
-  }
-
-  if (inputForm.password === "") {
-    alert("Please enter a password.");
-    return false;
-  }
-
-  if (inputForm.confirmPassword === "") {
-    alert("Please confirm your password.");
-    return false;
-  }
-
-  if (inputForm.password !== inputForm.confirmPassword) {
-    alert("Passwords do not match.");
-    return false;
-  }
-
-  return true;
 }
 
+function resetFormInputValues() {
+  document.getElementById("username-input").value = "";
+  document.getElementById("email-input").value = "";
+  document.getElementById("password-input").value = "";
+  document.getElementById("confirm-password-input").value = "";
+}
+
+function addError() {
+  Object.values(formFields).forEach((field) => {
+    field.classList.add("error");
+  });
+}
+
+function removeError() {
+  Object.values(formFields).forEach((field) => {
+    field.classList.remove("error");
+  });
+}
+
+function validateRegistrationForm() {
+  const form = getFormInputValues();
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if (
+    !form.username &&
+    !form.email &&
+    !form.password &&
+    !form.confirmPassword
+  ) {
+    alert("Please fill out the fields");
+    addError();
+    return false;
+  } else if (!form.username) {
+    alert("Please enter a username.");
+    formFields.username.classList.add("error");
+    return false;
+  } else if (!form.email) {
+    alert("Please enter an email address.");
+    formFields.email.classList.add("error");
+    return false;
+  } else if (!emailRegex.test(form.email)) {
+    alert("Please enter a valid email address.");
+    formFields.email.classList.add("error");
+    return false;
+  } else if (!form.password) {
+    alert("Please enter a password.");
+    formFields.password.classList.add("error");
+    return false;
+  } else if (form.password.length < 8) {
+    alert("Password must be at least 8 characters long.");
+    formFields.password.classList.add("error");
+    return false;
+  } else if (!form.confirmPassword) {
+    alert("Please confirm your password.");
+    formFields.confirmPassword.classList.add("error");
+    return false;
+  } else if (form.password !== form.confirmPassword) {
+    alert("Passwords do not match.");
+    formFields.password.classList.add("error");
+    formFields.confirmPassword.classList.add("error");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+Object.values(formFields).forEach((field) => {
+  field.addEventListener("focus", () => {
+    removeError();
+  });
+});
+
 function addUserAccount(payload) {
-  fetch(baseURL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  })
-    .then((response) => {
-      if (response.status === 200) {
-        return response.json();
-      } else {
-        throw new Error("Request failed");
-      }
+  return new Promise((resolve, reject) => {
+    fetch("./php/user-account.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
     })
-    .then((data) => {
-      console.log(data.data);
-    })
-    .catch((error) => {});
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === "success") {
+          alert(data.data);
+          resetFormInputValues();
+        } else {
+          alert(data.data);
+        }
+
+        resolve(data);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 }
 
 signUpButton.addEventListener("click", function () {
-  const form = {
-    username: document.getElementById("username-input").value,
-    email: document.getElementById("email-input").value,
-    password: document.getElementById("password-input").value,
-    confirmPassword: document.getElementById("confirm-password-input").value,
-  };
-
-  addUserAccount(form);
+  if (validateRegistrationForm()) {
+    const form = getFormInputValues();
+    addUserAccount(form);
+  }
 });
 
 // var socialMediaLinks = document.querySelectorAll(".social-media-links img");
