9b917e5fc12b1c9a6ad8ab0ef54ea543e7f57460
johnmichaelespiritu <johnmichael.espiritu.pixel8@gmail.com>
2023-06-27 11:57:03 +0800

    Add getUserAccount and addUserAccount functions.

--- scripts/api/userAccountAPI.js
+++ scripts/api/userAccountAPI.js
@@ -1,3 +1,41 @@
+export function getUserAccount() {
+  return new Promise((resolve, reject) => {
+    fetch("./php/user-account.php", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
+export function addUserAccount(payload) {
+  return new Promise((resolve, reject) => {
+    fetch("./php/user-account.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
 export function loginAccount(payload) {
   return new Promise((resolve, reject) => {
     fetch("./php/user-account.php", {
