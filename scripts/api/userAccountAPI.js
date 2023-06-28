46a8a64d74ea761d470abc9e407249fb10987a08
johnmichaelespiritu <johnmichael.espiritu.pixel8@gmail.com>
2023-06-28 22:29:58 +0800

    Add update function.

--- scripts/api/userAccountAPI.js
+++ scripts/api/userAccountAPI.js
@@ -55,3 +55,23 @@ export function loginAccount(payload) {
       });
   });
 }
+
+export function updateUserAccount(payload) {
+  return new Promise((resolve, reject) => {
+    fetch("./php/user-account.php", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+      credentials: "include",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
