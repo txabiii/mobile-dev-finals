8baa2dd36de6a30011e3cd19166c8967c2c4e6e1
johnmichaelespiritu <johnmichael.espiritu.pixel8@gmail.com>
2023-06-28 22:29:28 +0800

    Add update function.

--- php/user-account.php
+++ php/user-account.php
@@ -1,13 +1,15 @@
 <?php
 
 session_start();
+date_default_timezone_set('Asia/Singapore');
 
 require_once('db.php');
+require('vendor/autoload.php');
+
+use PHPMailer\PHPMailer\PHPMailer;
 
 header("Access-Control-Allow-Credentials: true");
-
 header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS");
-
 header("Access-Control-Allow-Headers: Content-Type");
 
 class API extends DB
@@ -32,6 +34,21 @@ class API extends DB
 		  } else {
 			echo json_encode(array('method' => 'GET', 'status' => 'failed', 'data' => 'Failed to retrieve users.'));
 		  }
+
+		// $user_id = $_SESSION['user_id'];
+
+		// $statement = $this->connection->prepare("SELECT user_id, username, email FROM user_accounts_tb WHERE user_id = ?");
+		// $statement->bind_param("s", $user_id);
+		// $statement->execute();
+		// $result = $statement->get_result();
+
+		// if ($result) {
+		// 	$users = $result->fetch_all(MYSQLI_ASSOC);
+
+		// 	echo json_encode(array('method' => 'GET', 'status' => 'success', 'data' => $users));
+		// } else {
+		// 	echo json_encode(array('method' => 'GET', 'status' => 'failed', 'data' => 'Failed to retrieve users.'));
+		// }
 	}
 
 	public function httpPost($payload)
@@ -55,11 +72,14 @@ class API extends DB
 					echo json_encode(array('method' => 'POST', 'status' => 'failed', 'data' => 'Username is already taken.'));
 				}
 			} else {
-				$add_user_account = "INSERT INTO user_accounts_tb (username, email, password) VALUES ('$username', '$email', '$hashed_password')";
+				$verification_code = $this->emailVerification($payload['email']);
+
+				$add_user_account = "INSERT INTO user_accounts_tb (username, email, password, verification_code) VALUES ('$username', '$email', '$hashed_password', '$verification_code')";
 				$new_account = $this->connection->query($add_user_account);
+				$user_id = $this->connection->insert_id;
 
 				if ($new_account) {
-					echo json_encode(array('method' => 'GET', 'status' => 'success', 'data' => 'Your account has been successfully created. You may now proceed to login.'));
+					echo json_encode(array('method' => 'GET', 'status' => 'success', 'data' => 'Your account has been successfully created. Please verify your email address.', 'user_id' => $user_id));
 				} else {
 					echo json_encode(array('method' => 'GET', 'status' => 'failed', 'data' => 'Account creation failed. Please try again.'));
 				}
@@ -95,12 +115,80 @@ class API extends DB
 
 	public function httpPut($payload)
 	{
-		
+		$user_id = $payload['user_id'];
+		$verification_code = $payload['verification_code'];
+		$date_time_now = date('Y-m-d H:i:s');
+
+		$statement = $this->connection->prepare("SELECT verification_code FROM user_accounts_tb WHERE user_id = ?");
+		$statement->bind_param("s", $user_id);
+		$statement->execute();
+		$result = $statement->get_result();
+		$row = $result->fetch_assoc();
+
+		if ($row && $verification_code === $row['verification_code']) {
+			$verify_account_query = "UPDATE user_accounts_tb SET email_verified_at = ? WHERE user_id = ?";
+			$statement = $this->connection->prepare($verify_account_query);
+			$statement->bind_param("ss", $date_time_now, $user_id);
+			$statement->execute();
+
+			echo json_encode(array('method' => 'PUT', 'status' => 'success', 'data' => 'You have successfully verified your account.'));
+		} else {
+			echo json_encode(array('method' => 'PUT', 'status' => 'failed', 'data' => 'Verification code failed.'));
+		}
+
+		$this->connection->close();
 	}
 
 	public function httpDelete($payload)
 	{
 
+	}
+
+	private function emailVerification($user_email)
+	{
+		$mail = new PHPMailer(true);
+
+		//Enable verbose debug output
+		$mail->SMTPDebug = 0; //SMTP::DEBUG_SERVER;
+
+		//Send using SMTP
+		$mail->isSMTP();
+
+		//Set the SMTP server to send through
+		$mail->Host = 'smtp.gmail.com';
+
+		//Enable SMTP authentication
+		$mail->SMTPAuth = true;
+
+		//SMTP username
+		$mail->Username = 'plantparenthoodassistant@gmail.com';
+
+		//SMTP password
+		$mail->Password = 'njjtgksukhvgsclc';
+
+		//Enable TLS encryption;
+		$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+
+		//TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+		$mail->Port = 587;
+
+		//Recipients
+		$mail->setFrom('plantparenthoodassistant@gmail.com', 'Plant Parenthood Assistant');
+
+		//Add a recipient
+		$mail->addAddress($user_email);
+
+		//Set email format to HTML
+		$mail->isHTML(true);
+
+		$verification_code = substr(number_format(time() * rand(), 0, '', ''), 0, 6);
+
+		$mail->Subject = 'Email verification';
+		$mail->Body    = '<p>Your verification code is: <b style="font-size: 30px;">' . $verification_code . '</b></p>';
+
+		$mail->send();
+
+		return $verification_code;
 	}
 }
 
