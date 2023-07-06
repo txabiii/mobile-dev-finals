1108bcc4862ca929758b8b23fec22e50571b05c8
johnmichaelespiritu <johnmichael.espiritu.pixel8@gmail.com>
2023-07-06 21:18:42 +0800

    Update.

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 /**
  * @type {string} - The base url of the backend PHP server
  */
-// export const SERVER_URL = "http://localhost/plant-parenthood/php";
-export const SERVER_URL = "https://trousered-frames.000webhostapp.com/php";
+export const SERVER_URL =
+  "https://plantparenthoodassistant.000webhostapp.com/php";
