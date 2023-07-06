3afeee120edaa421048f50cb3dc2958458187e61
txabiii <txabiguerrero2000@gmail.com>
2023-07-06 20:22:23 +0800

    New server url

--- scripts/config.js
+++ scripts/config.js
@@ -1,6 +1,8 @@
 /**
  * @type {URL} Backend endpoint a.k.a link to the server where the php files are running
  */
-export const SERVER_URL = 'http://localhost/plant-parenthood/php'; // Local server backend endpoint of Txabi
+// export const SERVER_URL = 'http://localhost/plant-parenthood/php'; // Local server backend endpoint of Txabi
 
 // export const SERVER_URL = 'https://trousered-frames.000webhostapp.com/php' // Production server #1 backend endpoint
+
+export const SERVER_URL = 'https://plantparenthoodassistant.000webhostapp.com/php' // Production server #2 backend endpoint
