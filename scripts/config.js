06161674562e583cd49a9667d4b2731d4035a76e
johnmichaelespiritu <johnmichael.espiritu.pixel8@gmail.com>
2023-07-07 21:13:18 +0800

    Update.

--- scripts/config.js
+++ scripts/config.js
@@ -1,6 +1,4 @@
 /**
  * @type {string} - The base url of the backend PHP server
  */
-//export const SERVER_URL = "https://plantparenthoodassistant.000webhostapp.com/php";
-
-export const SERVER_URL = 'http://localhost/plant-parenthood/php'
+export const SERVER_URL = "https://php.brayn.dev/php";
