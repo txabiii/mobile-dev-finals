0742c214ebd46c096bcd95e7537920633c5bed9e
txabiii <txabiguerrero2000@gmail.com>
2023-07-11 17:39:40 +0800

    Add Navbar

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,10 +1,29 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './page.module.scss'
+
+import logo from '../assets/logo.png'
 
 export default function Home() {
   return (
     <body>
-      <h1>Hello World</h1>
+      <nav className={styles.navbar}>
+        <Image src={logo} alt='logo' height={50} width={50}></Image>
+
+        <div className={styles.flexFiller}></div>
+
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>How to use</li>
+          <li>
+            <button>Download Now</button>
+          </li>
+        </ul>
+      </nav>
+
+      <main>
+        <section className={styles.heroSection}></section>
+      </main>
     </body>
   )
 }
