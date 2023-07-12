67c76527e77c1a52eb079b7dc91644d186030a8f
txabiii <txabiguerrero2000@gmail.com>
2023-07-12 10:17:09 +0800

    Add hero section

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.scss'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,10 +16,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&display=swap" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Rajdhani:wght@300;400;500;600;700&display=swap" />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -2,10 +2,29 @@ import Image from 'next/image'
 import styles from './page.module.scss'
 
 import logo from '../assets/logo.png'
+import android from '../assets/icons/android.svg'
+import gitHub from '../assets/icons/gitHub.svg'
+import homePage from '../assets/home.png'
+import notification from '../assets/notification.png'
+import reminderCard from '../assets/reminder-card.png'
+
+import { Barlow, Rajdhani } from 'next/font/google'
+
+const barlow = Barlow({
+  subsets: ['latin'],
+  weight: ['400', '500', '700', '800'],
+  variable: '--font-barlow',
+})
+
+const rajdhani = Rajdhani({
+  subsets: ['latin'],
+  weight: ['300', '400'],
+  variable: '--font-rajdhani',
+})
 
 export default function Home() {
   return (
-    <body>
+    <body className={`${barlow.variable} ${rajdhani.variable}`}>
       <nav className={styles.navbar}>
         <Image src={logo} alt='logo' height={50} width={50}></Image>
 
@@ -21,9 +40,47 @@ export default function Home() {
         </ul>
       </nav>
 
-      <main>
-        <section className={styles.heroSection}></section>
-      </main>
+      <section className={styles.hero}>
+          <div className={styles.content}>
+            <h1>Grow, Connect, and Thrive: <span>Plant Parenthood!</span></h1>
+
+            <p>Welcome to Plant Parenthood, the ultimate mobile app for plant owners. Simplify your plant care routine, never miss a watering again, and learn how to grow your plants with ease.</p>
+          
+            <div className={styles.buttonGroup}>
+              <button>
+                <Image src={android} alt='Android' width={36} height={20.62}></Image>
+                Download <span>APK</span>
+              </button>
+              <button>
+                <Image src={gitHub} alt='GitHub' width={36} height={36}></Image>
+                View on <span>GitHub</span></button>
+            </div>
+          </div>
+
+          <div className={styles.image}>
+            <Image 
+              src={homePage} 
+              alt='Home Page Screen Mockup' 
+              width={374} 
+              height={761.82}
+              className={styles.homePage}
+            ></Image>
+            <Image
+              src={notification}
+              alt='App notification popup'
+              width={320}
+              height={60}
+              className={styles.notification}
+            ></Image>
+            <Image
+              src={reminderCard}
+              alt='App reminder card'
+              width={305}
+              height={110}
+              className={styles.reminderCard}
+            ></Image>
+          </div>
+      </section>
     </body>
   )
 }
